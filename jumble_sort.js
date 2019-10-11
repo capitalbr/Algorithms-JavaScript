1f0ec4180b4010f329626d0e4e9d1b7371fd2a5f
Bradley <bbarnes.me@gmail.com>
2019-10-11 00:46:09 -0500

    Add jumble sort

--- jumble_sort.js
+++ jumble_sort.js
@@ -7,3 +7,27 @@
 // Example:
 // jumbleSort("hello") => "ehllo"
 // jumbleSort("hello", ['o', 'l', 'h', 'e']) => 'ollhe'
+
+
+function jumbleSort(string, alpha) {
+  if (!alpha)[
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+  ]
+
+  let str = string.slice().split("");
+  let sorted = false;
+    while (!sorted){
+      sorted = true;
+      for (let i = 0; i < str.length - 1; i++){
+        let first = alpha.indexOf(str[i]);
+        let second = alpha.indexOf(str[i + 1])
+        if (first > second){
+          sorted = false;
+          [str[i], str[i+1]] = [str[i+1], str[i]];
+        }
+    }
+  }
+  return str.join("");
+}
+console.log(jumbleSort("hello")) // ehllo
+console.log(jumbleSort("hello", "zyxwvutsrqponmlkjihgfedcba")) // ollhe
