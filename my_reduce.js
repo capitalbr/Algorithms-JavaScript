9afd51f960b48e841a80737b6a28c07c9e106611
Bradley <bbarnes.me@gmail.com>
2019-10-15 22:45:06 -0500

    Add my_reduce

--- my_reduce.js
+++ my_reduce.js
@@ -1,6 +1,33 @@
 // Write an `Array.prototype.myReduce(callback, acc)` method which takes a 
 // callback and an optional argument of a default accumulator. If myReduce only 
 // receives one argument, then use the first element of the array as the default 
-// accumulator. Use the `Array.prototype.myEach` method you defined above. Do 
-// NOT call in the built-in `Array.prototype.reduce` or `Array.prototype.forEach` 
-// methods.
+// accumulator.
+
+Array.prototype.myReduce = function (cb, a) {
+  let arr = this.slice();
+  if (!a){
+    a = arr.shift();
+  }
+
+  for (let i = 0; i < arr.length; i++){
+    a = cb(a, arr[i]);
+  }
+  return a;
+}
+
+
+console.log([1, 5, 6, 7].myReduce((x, y) => {
+  return x * y
+})) // 210
+
+console.log([1, 5, 6, 7].myReduce((x, y) => {
+  return x * y
+}, 10)) // 2100
+
+console.log([1, 5, 6, 7].myReduce((x, y) => {
+  return x - y
+})) // -17
+
+console.log([1, 5, 6, 7].myReduce((x, y) => {
+  return x - y
+}, 10)) // -9
