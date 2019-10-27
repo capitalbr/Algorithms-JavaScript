936815a474b35993bf7f9e9b00b559d0e4e25d32
Bradley <bbarnes.me@gmail.com>
2019-10-27 18:50:26 -0500

    Add median

--- median.js
+++ median.js
@@ -1,2 +1,17 @@
 // Write a function `median(arr)` that returns the median of elements in an array.
 // If the length is even, return the average of the middle two elements.
+
+
+const median = (arr) => {
+  if (!arr.length) return null;
+  let mid = Math.floor(arr.length / 2); 
+  let sorted = arr.sort();
+  if (arr.length % 2 === 0){
+    return (sorted[mid] + sorted[mid-1]) / 2
+  } else {
+    return sorted[mid];
+  }
+}
+
+console.log(median([5, 1, 3, 2, 4])) // 3
+console.log(median([35, 49, 13, 45, 67, 1])) // 40
