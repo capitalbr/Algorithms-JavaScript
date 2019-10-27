6b4d3609375627350102602579f4d74518d8ac01
Bradley <bbarnes.me@gmail.com>
2019-10-27 18:36:20 -0500

    Add merge_sort

--- merge_sort.js
+++ merge_sort.js
@@ -11,3 +11,43 @@
 // Split the array into left and right halves, then merge sort them recursively
 // until a base case is reached. Use a helper method, merge, to combine the
 // halves in sorted order, and return the merged array.
+
+
+Array.prototype.mergeSort = function (cb){
+  if (this.length <= 1) return this;
+
+  let mid = Math.floor(this.length / 2);
+
+  let left = this.slice(0, mid).mergeSort(cb);
+  let right = this.slice(mid).mergeSort(cb);
+
+  return merge(left, right, cb);
+}
+
+function merge(left, right, cb){
+  let merged = [];
+  if (!cb){
+    cb = (x, y) => {
+      return x > y ? 1 : y > x ? -1 : 0;
+    }
+  }
+
+  while (left.length && right.length){
+    switch(cb(left[0], right[0])){
+      case 1:
+        merged.push(right.shift());
+        break;
+      default:
+        merged.push(left.shift());
+    }
+  }
+
+  return merged.concat(left, right);
+}
+
+cb = (x, y) => { // just reverses them
+  return 1;
+}
+
+console.log([500000, 0, 300, 5, 3, 5, 222, 2].mergeSort(cb)) // [ 2, 222, 5, 3, 5, 300, 0, 500000 ]
+console.log([500000, 0, 300, 5, 3, 5, 222, 2].mergeSort()) // [ 0, 2, 3, 5, 5, 222, 300, 500000 ]
