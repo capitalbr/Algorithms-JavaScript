22a674149edac2f2175e633db6d67421199a6150
Bradley <bbarnes.me@gmail.com>
2019-10-27 16:33:46 -0500

    Add my_bind

--- my_bind.js
+++ my_bind.js
@@ -1,3 +1,35 @@
 // write a `Function.prototype.myBind(context)` method. It should return a copy 
 // of the original function, where `this` is set to `context`. It should allow 
 // arguments to the function to be passed both at bind time and call time.
+
+
+Function.prototype.myBind = function (context, ...bindArgs) {
+  const that = this;
+  return function (...callArgs) {
+    return that.apply(context, bindArgs.concat(callArgs));
+  };
+};
+
+
+class Hello{
+  constructor(){
+    this.answer = "Hello!"
+  }
+  sayHello(name1, name2){
+    return this.answer + name1 + name2;
+  }
+}
+
+class Goodbye{
+  constructor(){
+    this.answer = "Goodbye!"
+  }
+}
+
+let hello = new Hello;
+let goodbye = new Goodbye;
+
+let sayHelloFromGoodbye = hello.sayHello.bind(goodbye, "Fanny");
+console.log(sayHelloFromGoodbye("Fanny2"))
+sayHelloFromGoodbye = hello.sayHello.myBind(goodbye, "Bryan");
+console.log(sayHelloFromGoodbye("Bryan2"))
