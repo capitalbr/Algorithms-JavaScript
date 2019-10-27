4b8a0c281374f77ab850727dd92808f932941ca2
Bradley <bbarnes.me@gmail.com>
2019-10-27 17:19:49 -0500

    Add pig_latinify.js

--- pig_latinify.js
+++ pig_latinify.js
@@ -15,3 +15,46 @@
 //  3. Treat `qu` at the start of a word as a singular consonant.
 //      ex1. `queen` => `eenquay`
 //      ex2. `square` => `aresquay`
+
+
+function pigLatinifyWord(word){
+  let vowels = 'aeiou';
+  let front = "";
+  let i = 0;
+  for (; i < word.length; i++){
+    if (vowels.includes(word[i].toLowerCase())) break;
+    if (word[i+1] === "u") {
+      front += word[i] + "u";
+      // +2 because you need to go past the "u" idx so it will be removed from the front as well
+      i += 2;
+      break;
+    }
+    front += word[i];
+  }
+  return word.slice(i) + front + "ay";
+}
+
+// TESTS FOR HELPER
+// console.log(pigLatinifyWord("queen")); // => 'eenquay'
+// console.log(pigLatinifyWord("square")); // => 'aresquay'
+// console.log(pigLatinifyWord("apple")); // => 'appleay'
+// console.log(pigLatinifyWord("school")); // => 'oolschay'
+// console.log(pigLatinifyWord("zzwxxzbrizzled")); // => 'izzledzzwxxzbray'
+
+const pigLatinify = (sentence) => {
+  let converted = "";
+  let sentenceArr = sentence.split(" ");
+  sentenceArr.forEach((word, idx) => {
+    converted += pigLatinifyWord(word);
+    if (idx !== sentenceArr.length -1) converted += " ";
+  })
+  return converted;
+}
+
+let sentence = "Squares on a chess board with quEEns on them at school"
+
+console.log(pigLatinify(sentence));
+
+
+
+
