26dae8a36646189e8dd6ae29b40072ad2d7f96a9
Bradley <bbarnes.me@gmail.com>
2019-10-27 19:02:13 -0500

    Add my_flatten

--- my_flatten.js
+++ my_flatten.js
@@ -2,3 +2,12 @@
 // multi-dimensional array into a one-dimensional array.
 // Example:
 // [["a"], "b", ["c", "d", ["e"]]].myFlatten() => ["a", "b", "c", "d", "e"]
+
+
+Array.prototype.myFlatten = function (){
+  let flattened = [];
+  this.forEach(ele => Array.isArray(ele) ? flattened = flattened.concat(ele.myFlatten()) : flattened.push(ele));
+  return flattened;
+}
+
+console.log([[1], [2,3], [4, [5]], [6, [7, [8]], [9, [10], [11, 12, [[13]]]]]].myFlatten());
