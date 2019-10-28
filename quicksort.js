0704711c38fb3c37a2685230c85fab3b4cff9e9f
Bradley <bbarnes.me@gmail.com>
2019-10-27 21:21:29 -0500

    Add quicksort

--- quicksort.js
+++ quicksort.js
@@ -9,3 +9,25 @@
 // Choose a pivot element, then iterate over the rest of the array, moving the 
 // remaining elements on to the appropriate side of the pivot. Recursively quick 
 // sort each side of the array until a base case is reached. 
+
+
+Array.prototype.quickSort = function (cb){
+  if (this.length === 0) return [];
+  if (!cb) cb = (x, y) => {
+    return x < y ? -1 : x === y ? 0 : 1;
+  }
+
+  let pivot = this[0];
+  let left = this.slice(1).filter(ele => cb(ele, pivot) === -1);
+  let right = this.slice(1).filter(ele => cb(ele, pivot) !== -1);
+
+  return left.quickSort(cb).concat(pivot, right.quickSort(cb))
+}
+
+let cb = (x, y) => {
+  return x < y ? 1 : x > y ? -1 : 0;
+}
+
+console.log([23, 44, .37, 89.3, 100, 0, 2].quickSort())
+console.log([23, 44, .37, 89.3, 100, 0, 2].quickSort(cb))
+
