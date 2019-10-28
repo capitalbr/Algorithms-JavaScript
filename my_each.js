fcac1beef35678c730284a1455c111a62eac2637
Bradley <bbarnes.me@gmail.com>
2019-10-27 20:45:04 -0500

    Add my_each

--- my_each.js
+++ my_each.js
@@ -1,3 +1,22 @@
 // Write an `Array.prototype.myEach(callback)` method that invokes a callback
 // for every element in an array and returns undefined. Do NOT use the built-in
 // `Array.prototype.forEach`
+
+
+Array.prototype.myEach = function (cb){
+  for (let i = 0; i < this.length; i++){
+    cb(this[i], i);
+  }
+};
+
+[1, 4, 2, 5].myEach((ele) => {
+  console.log(ele * 2)
+});
+
+[1, 4, 2, 5].myEach((ele, idx) => {
+  console.log(ele * 2 + idx)
+});
+
+[1, 4, 2, 5].myEach((ele, idx) => {
+  console.log(`${ele} ${idx}`)
+});
