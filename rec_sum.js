5590fb734cb595760af2348b894530ce44a456fd
Bradley <bbarnes.me@gmail.com>
2019-10-27 20:28:44 -0500

    Add rec_sum

--- rec_sum.js
+++ rec_sum.js
@@ -1,2 +1,11 @@
 // Write a recursive function `recSum(numArr)` that returns the sum of all
 // elements in an array. Assume all elements are numbers.
+
+
+const recSum = (numArr) => {
+  if (!numArr.length) return 0;
+  if (numArr.length === 1) return numArr[0];
+  return numArr.pop() + recSum(numArr);
+}
+
+console.log(recSum([1, 4, 6, 199, 19]));
