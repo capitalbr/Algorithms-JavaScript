114ba130652d90c1a45979b8b160e0facf242ec2
Bradley <bbarnes.me@gmail.com>
2019-10-27 21:31:02 -0500

    Add my_call

--- my_call.js
+++ my_call.js
@@ -3,3 +3,32 @@
 // passed-in object as `this`, also passing the remaining arguments. Do NOT use
 // the built-in `Function.prototype.call` or `Function.prototype.apply` methods 
 // in your implementation.
+
+
+Function.prototype.myCall = function(context, ...args){
+  return this.bind(context, ...args)();
+}
+
+
+class Hello {
+  constructor() {
+    this.answer = "Hello!"
+  }
+  sayHello(name1, name2) {
+    return this.answer + name1 + name2;
+  }
+}
+
+class Goodbye {
+  constructor() {
+    this.answer = "Goodbye!"
+  }
+}
+
+let hello = new Hello;
+let goodbye = new Goodbye;
+
+let sayHelloFromGoodbye = hello.sayHello.call(goodbye, "Fanny", "Fanny2");
+console.log(sayHelloFromGoodbye)
+sayHelloFromGoodbye = hello.sayHello.myCall(goodbye, "Bryan", "Bryan2");
+console.log(sayHelloFromGoodbye)
