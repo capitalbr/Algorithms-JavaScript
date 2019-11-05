f3f1e0586f9a2a6e9c77caedeb468ddf92388dfb
Bradley <bbarnes.me@gmail.com>
2019-11-05 00:30:13 -0600

    Add my_slice

--- my_slice.js
+++ my_slice.js
@@ -4,3 +4,22 @@
 // ex. 
 // `abcde`.mySlice(2) => `cde`
 // `abcde`.mySlice(1, 3) => `bc`
+
+String.prototype.mySlice = function (startIdx = 0, endIdx = this.length){
+  let s = "";
+  for (let i = startIdx; i < endIdx && i < this.length; i++){
+    s += this[i];
+  }
+  return s;
+}
+
+console.log("testing".mySlice(1, 4)); // est
+console.log("testing".mySlice(1)); // esting
+console.log("testing".mySlice(4)); // ing
+console.log("testing".mySlice()); // testing
+console.log("testing".mySlice(0)); // testing
+console.log("testing".mySlice(3, 5)); // ti
+console.log(`abcde`.mySlice(2)); // cde
+console.log(`abcde`.mySlice(1, 3)) // bc
+console.log(`abcde`.mySlice(1, 40)) // bc
+
