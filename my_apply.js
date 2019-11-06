4005ce2610fc80a4885132e850b8d4d91a15e064
Bradley <bbarnes.me@gmail.com>
2019-11-06 02:40:36 -0600

    Add string_include_key

--- my_apply.js
+++ my_apply.js
@@ -3,3 +3,15 @@
 // passed-in object as `this`, also passing the arguments array. Do NOT use the
 // built-in `Function.prototype.apply` or `Function.prototype.call` methods in 
 // your implementation.
+
+
+switch("a"){
+  case "a":
+    console.log("hello");
+    break;
+  case "b":
+    console.log("goodbye");
+    break;
+  default:
+    console.log("default")
+}
--- string_include_key.js
+++ string_include_key.js
@@ -4,3 +4,19 @@
 //
 // stringIncludeKey("cadbpc", "abc") => true
 // stringIncludeKey("cba", "abc") => false
+
+
+const stringIncludeKey = (string, key) => {
+  if (!key.length) return true;
+  let letter = key[0];
+  let idx = string.indexOf(letter);
+  if (idx !== -1) {
+    return stringIncludeKey(string.slice(idx+1), key.slice(1))
+  }
+  return false;
+}
+
+console.log(stringIncludeKey("cadbpc", "abc")) // => true
+console.log(stringIncludeKey("cba", "abc")) // => false
+console.log(stringIncludeKey("abcdefg", "bcdfeg")) // => false
+console.log(stringIncludeKey("zx3h/5", "x/")) // => true
