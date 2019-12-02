1fb2051b2c499c7f15768ce512292ad23540d6e0
Bradley <bbarnes.me@gmail.com>
2019-12-01 23:06:20 -0600

    Add my_apply

--- my_apply.js
+++ my_apply.js
@@ -5,3 +5,39 @@
 // your implementation.
 
 
+Function.prototype.myApply = function (context, argsArr = []){
+  return this.bind(context, ...argsArr)()
+}
+
+Cat = function Cat(name) {
+  this.name = name;
+};
+
+Cat.prototype.sayHello = function () {
+  return this.name + " says hello!";
+};
+
+Cat.prototype.greetOne = function (otherCat) {
+  return this.name + " says hello to " + otherCat.name;
+};
+
+Cat.prototype.greetTwo = function (otherCat1, otherCat2) {
+  return this.name + " says hello to " + otherCat1.name + " and " +
+    otherCat2.name;
+};
+
+sally = new Cat("Sally");
+markov = new Cat("Markov");
+curie = new Cat("Curie");
+
+
+// invokes the function it is called on
+console.log(sally.greetOne.myApply(sally, [markov])); //"Sally says hello to Markov"
+
+
+//can take any number of arguments
+console.log(sally.greetTwo.myApply(sally, [markov, curie])); //"Sally says hello to Markov and Curie"
+
+
+// should call the function method style on the context
+console.log(sally.sayHello.myApply(markov)); //"Markov says hello!"
